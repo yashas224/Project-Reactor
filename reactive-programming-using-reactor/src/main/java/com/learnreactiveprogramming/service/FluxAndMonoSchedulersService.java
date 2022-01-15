12b14ce74073a25750aef8ecd42c794db139e308
yashas224 <71977329+yashas224@users.noreply.github.com>
2022-01-15 11:00:40 +0530

    update

--- reactive-programming-using-reactor/src/main/java/com/learnreactiveprogramming/service/FluxAndMonoSchedulersService.java
+++ reactive-programming-using-reactor/src/main/java/com/learnreactiveprogramming/service/FluxAndMonoSchedulersService.java
@@ -1,5 +1,8 @@
 package com.learnreactiveprogramming.service;
 
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
 import java.util.List;
 
 import static com.learnreactiveprogramming.util.CommonUtil.delay;
@@ -7,11 +10,32 @@ import static com.learnreactiveprogramming.util.CommonUtil.delay;
 public class FluxAndMonoSchedulersService {
 
     static List<String> namesList = List.of("alex", "ben", "chloe");
-    static List<String> namesList1 = List.of("adam", "jill", "jack");
 
     private String upperCase(String name) {
         delay(1000);
         return name.toUpperCase();
     }
 
+    // in reality flux might be coming from a Db of remote service Call
+    // Flux is a Publisher as it extends Publisher interface
+    public Flux<String> namesFlux() {
+        return Flux.fromIterable(namesList).log();
+    }
+
+    public Mono<String> nameMono() {
+        return Mono.just("yashas").log();
+    }
+
+    public static void main(String[] args) {
+        FluxAndMonoSchedulersService fluxAndMonoSchedulersService = new FluxAndMonoSchedulersService();
+        fluxAndMonoSchedulersService.namesFlux()
+                .subscribe(n -> System.out.println(n));
+
+
+        System.out.println("Mono Displaying");
+
+        fluxAndMonoSchedulersService.nameMono()
+                .subscribe(System.out::println);
+
+    }
 }
--- reactive-programming-using-reactor/src/test/java/com/learnreactiveprogramming/service/FluxAndMonoGeneratorServiceTest.java
+++ reactive-programming-using-reactor/src/test/java/com/learnreactiveprogramming/service/FluxAndMonoGeneratorServiceTest.java
@@ -1,4 +1,30 @@
 package com.learnreactiveprogramming.service;
 
+import org.junit.jupiter.api.Test;
+import reactor.test.StepVerifier;
+
 public class FluxAndMonoGeneratorServiceTest {
+    FluxAndMonoSchedulersService fluxAndMonoSchedulersService = new FluxAndMonoSchedulersService();
+
+    @Test
+    void namesFlux() {
+
+        var namesFlux = fluxAndMonoSchedulersService.namesFlux();
+
+        StepVerifier.create(namesFlux)
+//                .expectNext("alex", "ben", "chloe")
+//                .expectNextCount(3)
+                .expectNext("alex")
+                .expectNextCount(2)
+                .verifyComplete();
+
+    }
+
+    @Test
+    void nameMono() {
+        var nameMono=fluxAndMonoSchedulersService.nameMono();
+        StepVerifier.create(nameMono)
+                .expectNext("alex")
+                .verifyComplete();
+    }
 }
