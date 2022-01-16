4105cc2e72329238783466dcb0808931aa1d3e73
yashas224 <71977329+yashas224@users.noreply.github.com>
2022-01-16 13:49:28 +0530

    update

--- reactive-programming-using-reactor/src/main/java/com/learnreactiveprogramming/service/FluxAndMonoSchedulersService.java
+++ reactive-programming-using-reactor/src/main/java/com/learnreactiveprogramming/service/FluxAndMonoSchedulersService.java
@@ -3,7 +3,10 @@ package com.learnreactiveprogramming.service;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
+import java.time.Duration;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Random;
 
 import static com.learnreactiveprogramming.util.CommonUtil.delay;
 
@@ -22,8 +25,75 @@ public class FluxAndMonoSchedulersService {
         return Flux.fromIterable(namesList).log();
     }
 
+    public Flux<String> namesFlux_map() {
+        return Flux.fromIterable(namesList)
+                .map(String::toUpperCase).log();
+    }
+
+    public Flux<String> namesFlux_immutability() {
+        var namesFlux = Flux.fromIterable(namesList);
+        namesFlux.map(String::toUpperCase).log();
+        return namesFlux;
+    }
+
+
+    public Flux<String> namesFlux_filter(int strLength) {
+        return Flux.fromIterable(namesList)
+                .map(String::toUpperCase)
+                .filter(s -> s.length() > strLength)
+                .map(s -> s.length() + "-" + s)
+                .log();
+    }
+
+    // return individual Characters of each name
+    public Flux<String> namesFlux_flatMap(int strLength) {
+        return Flux.fromIterable(namesList)
+                .filter(s -> s.length() > strLength)
+                .flatMap(s -> splitStringToFlux(s)).log();
+    }
+
+    public Flux<String> splitStringToFlux(String s) {
+        return Flux.fromArray(s.split(""));
+    }
+
+    public Flux<String> namesFlux_flatMapAsync(int strLength) {
+        return Flux.fromIterable(namesList)
+                .filter(s -> s.length() > strLength)
+                .flatMap(s -> splitStringToFluxWithDelay(s)).log();
+    }
+
+    public Flux<String> splitStringToFluxWithDelay(String s) {
+        return Flux.fromArray(s.split(""))
+                .delayElements(Duration.ofMillis(new Random().nextInt(1000)));
+    }
+
+    public Flux<String> namesFlux_concatMap(int strLength) {
+        return Flux.fromIterable(namesList)
+                .filter(s -> s.length() > strLength)
+                .concatMap(s -> splitStringToFluxWithDelay(s)).log();
+    }
+
     public Mono<String> nameMono() {
         return Mono.just("yashas").log();
+    }
+
+    public Mono<List<String>> nameMono_flatmap() {
+        return Mono.just("yashas")
+                .flatMap(s -> Mono.just(Arrays.asList(s.split(""))))
+                .log();
+    }
+
+    public Flux<String> nameMono_flatmapMany() {
+        return Mono.just("yashas")
+                .flatMapMany(s -> splitStringToFlux(s))
+                .log();
+    }
+
+    public Flux<String> namesFlux_transform(int strLength) {
+        return Flux.fromIterable(namesList)
+                .transform()
+                .filter(s -> s.length() > strLength)
+                .flatMap(s -> splitStringToFlux(s)).log();
     }
 
     public static void main(String[] args) {
@@ -31,11 +101,11 @@ public class FluxAndMonoSchedulersService {
         fluxAndMonoSchedulersService.namesFlux()
                 .subscribe(n -> System.out.println(n));
 
-
         System.out.println("Mono Displaying");
 
         fluxAndMonoSchedulersService.nameMono()
                 .subscribe(System.out::println);
-
     }
+
+
 }
--- reactive-programming-using-reactor/src/test/java/com/learnreactiveprogramming/service/FluxAndMonoGeneratorServiceTest.java
+++ reactive-programming-using-reactor/src/test/java/com/learnreactiveprogramming/service/FluxAndMonoGeneratorServiceTest.java
@@ -3,6 +3,9 @@ package com.learnreactiveprogramming.service;
 import org.junit.jupiter.api.Test;
 import reactor.test.StepVerifier;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class FluxAndMonoGeneratorServiceTest {
     FluxAndMonoSchedulersService fluxAndMonoSchedulersService = new FluxAndMonoSchedulersService();
 
@@ -22,9 +25,76 @@ public class FluxAndMonoGeneratorServiceTest {
 
     @Test
     void nameMono() {
-        var nameMono=fluxAndMonoSchedulersService.nameMono();
+        var nameMono = fluxAndMonoSchedulersService.nameMono();
         StepVerifier.create(nameMono)
                 .expectNext("alex")
+                .verifyComplete();
+    }
+
+
+    @Test
+    void namesFlux_map() {
+        var namesFlux_map = fluxAndMonoSchedulersService.namesFlux_map();
+        StepVerifier.create(namesFlux_map)
+                .expectNext("ALEX", "BEN", "CHLOE")
+                .verifyComplete();
+    }
+
+    @Test
+    void namesFlux_immutability() {
+        var namesFlux_map = fluxAndMonoSchedulersService.namesFlux_immutability();
+        StepVerifier.create(namesFlux_map)
+                .expectNext("alex", "ben", "chloe")
+                .verifyComplete();
+    }
+
+    @Test
+    void namesFlux_filter() {
+        var namesFlux = fluxAndMonoSchedulersService.namesFlux_filter(3);
+        StepVerifier.create(namesFlux)
+                .expectNext("4-ALEX", "5-CHLOE")
+                .verifyComplete();
+    }
+
+    @Test
+    void namesFlux_flatMap() {
+        var namesFlux = fluxAndMonoSchedulersService.namesFlux_flatMap(3);
+
+        StepVerifier.create(namesFlux)
+                .expectNext("a", "l", "e", "x", "c", "h", "l", "o", "e")
+                .verifyComplete();
+    }
+
+    @Test
+    void namesFlux_flatMapAsync() {
+        var flux = fluxAndMonoSchedulersService.namesFlux_flatMapAsync(3);
+
+        StepVerifier.create(flux)
+                .expectNextCount(9)
+                .verifyComplete();
+    }
+
+    @Test
+    void namesFlux_concatMap() {
+        var flux = fluxAndMonoSchedulersService.namesFlux_concatMap(3);
+        StepVerifier.create(flux)
+                .expectNext("a", "l", "e", "x", "c", "h", "l", "o", "e")
+                .verifyComplete();
+    }
+
+    @Test
+    void nameMono_flatmap() {
+        var mono = fluxAndMonoSchedulersService.nameMono_flatmap();
+        StepVerifier.create(mono)
+                .expectNext(List.of("y", "a", "s", "h", "a", "s"))
+                .verifyComplete();
+    }
+
+    @Test
+    void nameMono_flatmapMany() {
+        var mono = fluxAndMonoSchedulersService.nameMono_flatmapMany();
+        StepVerifier.create(mono)
+                .expectNext("y", "a", "s", "h", "a", "s")
                 .verifyComplete();
     }
 }
